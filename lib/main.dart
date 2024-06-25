e0cfab8fbc9737e5fe79aedcad2e3ad6c7eaf048
zecht12 <gilangprimae13@gmail.com>
2024-06-25 14:39:10 +0700

    add app icons

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Aplikasi Jual Beli Buku Bekas',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
